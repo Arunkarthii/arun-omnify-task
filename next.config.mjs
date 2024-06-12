1d109b63d1ce15b913865ebd64323bbfbeaeb9ad
Arunkarthii <arunkarthikeyan2752@gmail.com>
2024-06-12 23:55:15 +0530

    changes

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,11 @@
 const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
-    }
+    },
+    reactStrictMode: true,
+    images: {
+        domains: ['https://arun-omnify-task.vercel.app/'], // Replace with your allowed domains
+    },
 };
 
 export default nextConfig;
